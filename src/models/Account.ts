bcccd351dfe3186f262e0dbfbf14e6aa78015e8c
guilhermer_mesquita@hotmail.com <guilhermer_mesquita@hotmail.com>
2023-04-25 22:48:22 -0400

    teste

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,40 +1,40 @@
-export class Account {
-    constructor(
-        private id: string,
-        private balance: number,
-        private ownerId: string,
-        private createdAt: string
-    ) {}
+// export class Account {
+//     constructor(
+//         private id: string,
+//         private balance: number,
+//         private ownerId: string,
+//         private createdAt: string
+//     ) {}
 
-    public getId(): string {
-        return this.id
-    }
+//     public getId(): string {
+//         return this.id
+//     }
 
-    public setId(value: string): void {
-        this.id = value
-    }
+//     public setId(value: string): void {
+//         this.id = value
+//     }
 
-    public getBalance(): number {
-        return this.balance
-    }
+//     public getBalance(): number {
+//         return this.balance
+//     }
 
-    public setBalance(value: number): void {
-        this.balance = value
-    }
+//     public setBalance(value: number): void {
+//         this.balance = value
+//     }
 
-    public getOwnerId(): string {
-        return this.ownerId
-    }
+//     public getOwnerId(): string {
+//         return this.ownerId
+//     }
 
-    public setOwnerId(value: string): void {
-        this.ownerId = value
-    }
+//     public setOwnerId(value: string): void {
+//         this.ownerId = value
+//     }
 
-    public getCreatedAt(): string {
-        return this.createdAt
-    }
+//     public getCreatedAt(): string {
+//         return this.createdAt
+//     }
 
-    public setCreatedAt(value: string): void {
-        this.createdAt = value
-    }
-}
+//     public setCreatedAt(value: string): void {
+//         this.createdAt = value
+//     }
+// }
